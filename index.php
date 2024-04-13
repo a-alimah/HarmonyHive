82b30f44c6362c898772e80036f1ad631654775f
Aalimah Afolabi <123120640+a-alimah@users.noreply.github.com>
2024-04-13 11:39:29 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Login </title> 
+    <link rel="stylesheet" href="css/register.css">
+  </head>
+<body>
+  <div class="wrapper">
+    <h2>Welcome To HarmonyHive</h2>
+    
+      <a href="login/signup.php" ><button class="input-box button">Proceed To SignUp</button></a>
+  </div>
+
+</body>
+</html>
+
+	
