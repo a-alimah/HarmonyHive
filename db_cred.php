39ef53b406973d3dbdd76455f7a7b8cddabd32ab
Aalimah Afolabi <123120640+a-alimah@users.noreply.github.com>
2024-04-13 11:52:47 +0000

    Add files via upload

---
+++ db_cred.php
@@ -0,0 +1,8 @@
+<?php
+//Database credentials
+define("SERVER","localhost");
+define("USERNAME","harmorny_hive");
+define("PASSWD","2J5L3nFpRSvSsr248c5e");
+define("DATABASE","harmorny_hive");
+
+?>
